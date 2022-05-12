d1d8931f085215177dd2034f2301e574e4fc642c
Tom Dodds <thomas@creativelycomon.co.uk>
2022-05-12 17:08:55 +0200

    added all the backdrop selectors and disabled mouse interactions

--- content.js
+++ content.js
@@ -1,14 +1,23 @@
 
 const cssModifications = [
-    ".backdrop-2 { display: none !important; }",
+    ".tiktok-dialog { display: none !important; }",
     ".footer-guide { display: none !important; }",
-    ".tiktok-bottom { bottom: 23px !important; }",
+    ".tiktok-bottom { bottom: 0px !important;}",
+    ".tiktok-bottom-left { pointer-events: none !important; }",
+    ".tiktok-bottom a { pointer-events: none !important; }",
     ".home-header { display: none !important; }",
     ".footer > :nth-child(3) { display: none !important; }",
     ".footer > :nth-child(4) { display: none !important; }",
     ".footer > :nth-child(5) { display: none !important; }",
-    ".footer { justify-content: space-around !important; }"
+    ".footer { justify-content: space-around !important; }",
+    ".heart-twink, .tiktok-toolbar-comment, .tiktok-toolbar-share {pointer-events: none !important;"
 ];
+
+const generateModalSelectors = () => {
+    for (let index = 0; index < 1000; index++) {
+        cssModifications.push(`.backdrop-${index} { display: none !important; },`)
+    }
+}
 
 function loadCSS(css) {
     var head = document.head || document.getElementsByTagName("head")[0];
@@ -17,10 +26,11 @@ function loadCSS(css) {
     head.appendChild(style);
 }
 
-const alterDom = () => {
+const applDomAlterations = () => {
     for (const mod of cssModifications) {
         loadCSS(mod);
     }
 }
 
-alterDom();
+generateModalSelectors();
+applDomAlterations();
