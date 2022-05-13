d984e282bf95ef337dfb14eae80ead497d86c79a
Tom Dodds <thomas@creativelycomon.co.uk>
2022-05-13 10:03:26 +0200

    fixes bar along bottom

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 
 const cssModifications = [
     ".tiktok-dialog { display: none !important; }",
-    ".footer-guide { display: none !important; }",
+    ".footer-guide { visibility: hidden !important; }",
     ".captcha_verify_container { display: none !important; }",
     // ".tiktok-bottom { bottom: var(--app-menu-height) !important;}",
-    ".tiktok-bottom { bottom: 23px !important;}",
+    ".tiktok-bottom { bottom: 0px !important;}",
     ".tiktok-bottom-left { pointer-events: none !important; }",
     ".tiktok-bottom a { pointer-events: none !important; }",
     ".home-header { display: none !important; }",
@@ -12,19 +12,12 @@ const cssModifications = [
     ".footer > :nth-child(4) { display: none !important; }",
     ".footer > :nth-child(5) { display: none !important; }",
     ".footer { justify-content: space-around !important; }",
-    ".heart-twink, .tiktok-toolbar-comment, .tiktok-toolbar-share {pointer-events: none !important;"
+    ".heart-twink, .tiktok-toolbar-comment, .tiktok-toolbar-share {pointer-events: none !important; }",
+    "tiktok-cookie-banner {display: none !important; }",
+    ".main { height: calc(100% - 49px) !important;}",
+    "@media (min-width: 1150px) { .main { height: calc(100% - 64px) !important; }}",
+    "@media (min-width: 1440px) { .main { height: calc(100% - 72px) !important; }}",
 ];
-
-
-// 72 1440
-
-// 64 1150
-
-// 56 800
-
-// 49
-
-
 
 const generateModalSelectors = () => {
     for (let index = 0; index < 1000; index++) {
